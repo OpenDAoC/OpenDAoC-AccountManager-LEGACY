eefb2a39448d1cced51477c5c1eefaa319329d2f
clait <stefanomus@fastmail.com>
2022-01-15 00:05:39 +0000

    .env like no tomorrow

--- config.php
+++ config.php
@@ -1,11 +1,8 @@
 <?php
-# CLIENT ID
-# https://i.imgur.com/GHI2ts5.png (screenshot)
-$client_id = "931670383025856563";
 
-# CLIENT SECRET
-# https://i.imgur.com/r5dYANR.png (screenshot)
-$secret_id = "6rb00a86WzjW3RBP7L_j6oF-ompVKtGz";
+use DotEnv;
+
+(new DotEnv(__DIR__ . '/.env'))->load();
 
 # SCOPES SEPARATED BY SPACE
 # example: identify email guilds connections  
@@ -14,7 +11,7 @@ $scopes = "identify";
 # REDIRECT URL
 # example: https://mydomain.com/includes/login.php
 # example: https://mydomain.com/test/includes/login.php
-$redirect_url = "http://localhost:8080/includes/login.php";
+
 
 # IMPORTANT READ THIS:
 # - Set the `$bot_token` to your bot token if you want to use guilds.join scope to add a member to your server
@@ -24,13 +21,17 @@ $redirect_url = "http://localhost:8080/includes/login.php";
 # https://i.imgur.com/2tlOI4t.png (screenshot)
 $bot_token = null;
 
-
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-const DB_SERVER = '172.19.208.1';
-const DB_USERNAME = 'atlas';
-const DB_PASSWORD = 'atlas';
-const DB_NAME = 'atlas';
+
+define("DB_SERVER", getenv('DB_SERVER'));
+define("DB_USERNAME", getenv('DB_USERNAME'));
+define("DB_PASSWORD", getenv('DB_PASSWORD'));
+define("DB_NAME", getenv('DB_NAME'));
+$redirect_url = getenv('REDIRECT_URL');
+$client_id = getenv('CLIENT_ID');
+$secret_id = getenv('SECRET_ID');
+
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
--- index.php
+++ index.php
@@ -84,6 +84,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     }
 
+
     // Close connection
     mysqli_close($link);
 }
