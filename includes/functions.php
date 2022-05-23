fa24bb738cd68869420645df46eb86b09d454af1
clait <stefanomus@fastmail.com>
2022-05-23 13:39:27 +0100

    Handling null discordi - dunno why this should would have it null

--- includes/functions.php
+++ includes/functions.php
@@ -72,6 +72,10 @@ function cryptPassword($pass)
 
 // Getting the GameAccount
 function getGameAccount(string $DiscordID){
+    if (empty($DiscordID)){
+        return "";
+    }
+
     $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
     $sql = "SELECT Name FROM account WHERE DiscordID = ?";
 
